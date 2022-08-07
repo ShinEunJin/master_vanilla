fb8ba30c2be4caeb4771da1116284ff0329435b2
ShinEunJin <sineun5501@naver.com>
2022-08-07 14:17:09 +0900

    spa routing

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
-const App = document.getElementById("app");
+const Page = `<h1>Page</h1>`;
 
-window.addEventListener("load", () => console.log("hihi"));
+const app = document.getElementById("app");
+
+const home = document.createElement("h1");
+home.innerText = "Home";
+app.appendChild(home);
+
+const page = document.createElement("span");
+page.style.cursor = "pointer";
+page.setAttribute("route", "page");
+page.innerText = "page";
+app.appendChild(page);
+
+window.onload = () => {
+  page.addEventListener("click", (e) => {
+    const path = e.target.getAttribute("route");
+
+    window.history.pushState(
+      { page: 1 },
+      path,
+      `${window.location.origin}/${path}`
+    );
+    app.innerHTML = Page;
+  });
+
+  window.addEventListener("popstate", (e) => {
+    console.log(window.history);
+  });
+};
