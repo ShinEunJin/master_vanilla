469698e5f4495949a00ee34f9b222ba1065cfe8f
ShinEunJin <sineun5501@naver.com>
2023-05-14 19:18:35 +0900

    promise 정리

--- src/asynchronism/promise.js
+++ src/asynchronism/promise.js
@@ -63,3 +63,22 @@
 
 //   document.body.append(img);
 // });
+
+// ===== 정리 =====
+
+// new Promise((resolve, reject) => {...})
+
+// 프로미스의 콜백안에서 resolve나 reject를 호출하면 바로 작업 마무리.
+
+// (resolve는 fulfilled, reject는 rejected 상태로 처리)
+
+// 이후 then(또는 catch 또는 finally)을 사용하여
+
+// resolve(값)의 인자('값')가 then의 매개변수가 된다.
+
+// const promise = new Promise((resolve, reject) => {
+//     ...
+//     resolve(값)
+// })
+
+// promise.then(res (= 값) => res를 활용할 수 있는 코드)
