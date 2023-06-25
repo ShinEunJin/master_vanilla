22f33639c64fe1629d7e3a7665c0eff12067af08
ShinEunJin <sineun5501@naver.com>
2023-06-25 11:52:35 +0900

    Object.defineProperty 사용

--- src/pattern/observer.js
+++ src/pattern/observer.js
@@ -1,7 +1,15 @@
 class Publisher {
-  constructor() {
+  constructor(state) {
     this.observers = new Set();
-    this.state = {};
+    this.state = state;
+
+    Object.keys(state).forEach((key) => {
+      Object.defineProperty(this, key, {
+        get() {
+          return this.state[key];
+        },
+      });
+    });
   }
 
   register(subscribe) {
@@ -33,20 +41,20 @@ const $list1 = document.querySelector(".list-1");
 const $list2 = document.querySelector(".list-2");
 const $list3 = document.querySelector(".list-3");
 
-const publisher = new Publisher();
+const publisher = new Publisher({ price: 0 });
 
 const subscriberList1 = new Subscriber(() => {
-  $list1.innerText = +$list1.dataset.price * (publisher.state.price || 0);
+  $list1.innerText = +$list1.dataset.price * publisher.price;
 });
 subscriberList1.subscribe(publisher);
 
 const subscriberList2 = new Subscriber(() => {
-  $list2.innerText = +$list2.dataset.price * (publisher.state.price || 0);
+  $list2.innerText = +$list2.dataset.price * publisher.price;
 });
 subscriberList2.subscribe(publisher);
 
 const subscriberList3 = new Subscriber(() => {
-  $list3.innerText = +$list3.dataset.price * (publisher.state.price || 0);
+  $list3.innerText = +$list3.dataset.price * (publisher.price || 0);
 });
 subscriberList3.subscribe(publisher);
 
